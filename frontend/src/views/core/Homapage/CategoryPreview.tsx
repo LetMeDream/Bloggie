e1926640a49e913254a45869d55855db90127c6c
mariann <cesinelisa@gmail.com>
2025-05-10 17:27:05 -0500

    styles(CategoryPreview.tsx): add cursor pointer and shadow, adjust layout card

--- frontend/src/views/core/Homapage/CategoryPreview.tsx
+++ frontend/src/views/core/Homapage/CategoryPreview.tsx
@@ -6,28 +6,28 @@ interface Category {
 
   const CategoryPreview = ({ category }: { category: Category[] }) => {
   return (
-    <div className="w-full flex flex-row md:justify-start lg:justify-start flex-wrap gap-4 mt-3 mb-2 sm:flex-col items-center justify-center">
+    <div className="w-full flex flex-row md:justify-start lg:justify-start flex-wrap gap-3 mt-3 mb-2 sm:flex-col items-center justify-center">
       {category.map((cat) => (
         <div
           key={cat.id}
-          className="bg-transparent w-[215px] h-[190px] shadow-md rounded-lg flex-shrink flex flex-col items-center justify-between"
+          className="w-[175px] cursor-pointer h-[160px] border border-color-gray rounded-lg flex flex-col items-center bg-transparent justify-between transition-shadow duration-300 hover:shadow-lg"
         >
-          <div className="w-full min-w-[50%] h-[60%] rounded-t-lg">
+          <div className="w-full rounded-t-lg">
             <img
-              className="w-full h-full rounded-t-lg object-cover"
+              className="w-full h-[90px] rounded-t-lg object-cover"
               src={cat.image}
               alt={cat.title}
             />
           </div>
            {/* Contenido */}
-           <div className="w-full h-[40%] flex flex-col items-center justify-center bg-white rounded-b-lg">
-            <h5 className="text-center mt-3 mb-0">
+            <div className="w-full h-[40%] flex flex-col items-center justify-center bg-white rounded-b-lg leading-tight">
+             <p className="text-center pt-1 font-sans mb-0 text-[17px] px-2 font-semibold text-gray-800">
               {cat.title}
-            </h5>
-            <p className="text-center ">
+             </p>
+             <p className="text-center ">
               1 article
-            </p>
-          </div>
+             </p>
+            </div>
         </div>
       ))}
     </div>
