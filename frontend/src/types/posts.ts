7e283b7a85435c8dfae0540096878f75fcebec52
Raul <raulalfonzo66@gmail.com>
2025-09-04 23:39:24 -0400

    Chore: create types for Category and AddPost

--- frontend/src/types/posts.ts
+++ frontend/src/types/posts.ts
@@ -1,3 +1,12 @@
+
+export interface CategoryType {
+  id: number;
+  image: string;
+  title: string;
+  post_count: number;
+}
+
+/* Utilizado, por ejemplo, en el componente Details al momento de tipar lo Post recibidos. */
 export interface Post {
   id: number;
   title: string;
@@ -9,9 +18,13 @@ export interface Post {
   slug?: string;
 }
 
-export interface CategoryType {
-  id: number;
-  image: string;
-  title: string;
-  post_count: number;
+/* Utilizado en el componente de creación de post. AddPost. */
+export type AddPostForm = {
+  title: string
+  description: string
+  status: 'draft' | 'published'
+  tags: string[]
+  category: string
+  user: string
+  image: string
 }
