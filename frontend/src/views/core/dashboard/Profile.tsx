9cfd1382aec73f0bebaf476aacca1163d0c8ba97
Raul <raulalfonzo66@gmail.com>
2025-09-28 19:41:21 -0400

    style: modify broken styles

--- frontend/src/views/core/dashboard/Profile.tsx
+++ frontend/src/views/core/dashboard/Profile.tsx
@@ -7,7 +7,7 @@ function Profile () {
   return (
     <>
       <Header />
-      <section className='pt-5 pb-5'>
+      <section className='pt-20 pb-5'>
         <div className='container'>
           <div className='row mt-0 mt-md-4'>
             <div className='col-lg-12 col-md-8 col-12'>
